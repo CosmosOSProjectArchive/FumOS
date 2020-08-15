60d920027caa94738d66c86f16072b67f8107976
tofudd <bartlomiej.tota@outlook.com>
2020-08-15 22:59:50 +0200

    Project files

---
+++ FumOS/Kernel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace CosmosTest
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+        }
+
+        protected override void Run()
+        {
+            Piano();
+        }
+
+        private void Piano()
+        {
+            while (true)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    // First octave
+
+                    // C4
+                    case ConsoleKey.Z:
+                        Console.Beep(261, 100);
+                        break;
+
+                    // C4#
+                    case ConsoleKey.S:
+                        Console.Beep(277, 100);
+                        break;
+
+                    // D4
+                    case ConsoleKey.X:
+                        Console.Beep(293, 100);
+                        break;
+
+                    // D4#
+                    case ConsoleKey.D:
+                        Console.Beep(311, 100);
+                        break;
+
+                    // E4
+                    case ConsoleKey.C:
+                        Console.Beep(329, 100);
+                        break;
+
+                    // F4
+                    case ConsoleKey.V:
+                        Console.Beep(349, 100);
+                        break;
+
+                    // F4#
+                    case ConsoleKey.G:
+                        Console.Beep(369, 100);
+                        break;
+
+                    // G4
+                    case ConsoleKey.B:
+                        Console.Beep(391, 100);
+                        break;
+
+                    // G4#
+                    case ConsoleKey.H:
+                        Console.Beep(415, 100);
+                        break;
+
+                    // A4
+                    case ConsoleKey.N:
+                        Console.Beep(440, 100);
+                        break;
+
+                    // A4#
+                    case ConsoleKey.J:
+                        Console.Beep(466, 100);
+                        break;
+
+                    // B4
+                    case ConsoleKey.M:
+                        Console.Beep(493, 100);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fumOS
 Touhou plush and Disk Operating System
+
+## About
+I have no idea what I doing but it's fun.
+
+## Details
+Bootable .NET app created with [this](https://github.com/CosmosOS/Cosmos).
